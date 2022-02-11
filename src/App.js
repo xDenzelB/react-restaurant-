e7eff1cd439ddacf470d4a354ecc2ab8949c83b5
Denzel <dbartolaba123@gmail.com>
2022-02-11 09:39:13 -0800

    Added in imports for App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, NavLink, Route, Redirect, } from 'react-router-dom';
+import AuthPage from './AuthPage';
+import DetailPage from './DetailPage';
+import ListPage from './ListPage';
+import CreatePage from './CreatePage'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
