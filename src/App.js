1c39f7583f2fda2d457a8b154568365d7070d278
Denzel <dbartolaba123@gmail.com>
2022-02-11 09:52:43 -0800

    Added in app.js routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,42 @@ import AuthPage from './AuthPage';
 import DetailPage from './DetailPage';
 import ListPage from './ListPage';
 import CreatePage from './CreatePage'; 
+import { getUser, logout } from './services/fetch-Utils';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetch() {
+      const user = await getUser();
+
+      if (user) setUser(user);
+    }
+
+    fetch();
+  }, []);
+
+  async function handleLogout() {
+    await logout();
+    setUser(null);
+  }
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          {
+            user && 
+            <div>
+              <NavLink activeClassName='link-edit' to='/restaurants'>List of Restaurants</NavLink>
+              <NavLink activeClassName='link-edit' to='/create'>Create Restaurant</NavLink>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          }
+        </header>
+     
+      </div>
+    </Router>
   );
 }
 
