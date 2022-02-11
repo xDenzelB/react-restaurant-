dc8b595cd53f551bc19e4671e97cf30466a91209
Denzel <dbartolaba123@gmail.com>
2022-02-11 10:48:08 -0800

    created Resturant.js

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -3,7 +3,26 @@ import { useRouteMatch } from 'react-router-dom';
 import { getRestaurantById } from './services/fetch-Utils';
 
 export default function DetailPage() {
+  const [restaurant, setRestaurant] = useState({});
+  const match = useRouteMatch();
+
+  useEffect(() => {
+    async function fetch() {
+      const restaurants = await getRestaurantById(match.params.id);
+
+      setRestaurant(restaurants);
+
+    }
+
+    fetch();
+  }, [match.params.id]);
   return (
-    <div>DetailPage</div>
+    <div className='details'>
+      <h1>{restaurant.name}</h1>
+      <h2>An {restaurant.type} Restaurant located in {restaurant.location} with a price rating of {restaurant.price}</h2>
+      <p>
+        {restaurant.description}
+      </p>
+    </div>
   );
 }
---
+++ src/Restaurant.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+export default function Restaurant({ restaurant }) {
+  return (
+    <Link to={`/restaurants/${restaurant.id}`}>
+      <div className='food'>
+        <h1>{restaurant.name}</h1>
+        <h2>An {restaurant.type} Restaurant located in {restaurant.location} with a price rating of {restaurant.price}</h2>
+
+      </div>
+    </Link>
+  );
+
+}
+  
